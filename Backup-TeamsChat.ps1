3f299249927118dece52daecc9e54c1ae570936b
Lee Ford <fordyman@gmail.com>
2021-06-16 10:34:40 +0100

    Improved formatting

--- Backup-TeamsChat.ps1
+++ Backup-TeamsChat.ps1
@@ -3,7 +3,7 @@
 .SYNOPSIS
  
     Backup-TeamsChat.ps1 - Backup Teams chat messages
-    https://www.lee-ford.co.uk/backup-teamschat
+    https://github.com/leeford/Backup-TeamsChat
  
 .DESCRIPTION
     Author: Lee Ford
@@ -27,9 +27,7 @@
 #>
 
 Param (
-
     [Parameter(mandatory = $true)][string]$Path
-
 )
 
 # Application (client) ID, resource and scope
@@ -40,12 +38,10 @@ $script:tenantId = "common"
 function Invoke-GraphAPICall {
 
     param (
-
         [Parameter(mandatory = $true)][uri]$URI,
         [Parameter(mandatory = $false)][switch]$WriteStatus,
         [Parameter(mandatory = $false)][string]$Method,
         [Parameter(mandatory = $false)][string]$Body
-
     )
 
     # Is method speficied (if not assume GET)
@@ -55,58 +51,37 @@ function Invoke-GraphAPICall {
     $currentEpoch = [int][double]::Parse((Get-Date (get-date).ToUniversalTime() -UFormat %s))
 
     if ($currentEpoch -gt [int]$script:token.expires_on) {
-
         Refresh-UserToken
-
     }
 
     $Headers = @{"Authorization" = "Bearer $($script:token.access_token)" }
-
     $currentUri = $URI
-
     $content = while (-not [string]::IsNullOrEmpty($currentUri)) {
-
         # API Call
         $apiCall = try {
-            
             Invoke-RestMethod -Method $method -Uri $currentUri -ContentType "application/json" -Headers $Headers -Body $body -ResponseHeadersVariable script:responseHeaders
-
         }
         catch {
-            
             $errorMessage = $_.ErrorDetails.Message | ConvertFrom-Json
-
         }
         
         $currentUri = $null
-    
         if ($apiCall) {
-    
             # Check if any data is left
             if ($apiCall.'@odata.count' -gt 0) {
-
                 $currentUri = $apiCall.'@odata.nextLink'
-
             }
-
             $apiCall
-
         }
-
     }
 
     if ($WriteStatus) {
-
         # If error returned
         if ($errorMessage) {
-
             Write-Host "FAILED $($errormessage.error.message)" -ForegroundColor Red
-
         }
         else {
-
             Write-Host "SUCCESS" -ForegroundColor Green
-
         }
         
     }
@@ -118,16 +93,11 @@ function Invoke-GraphAPICall {
 function Get-UserToken {
 
     $script:token = $null
-
     $resource = "https://graph.microsoft.com/"
-
     $codeBody = @{ 
-
         resource  = $resource
         client_id = $script:clientId
         scope     = $script:scope
-        
-
     }
 
     # Get OAuth Code
@@ -137,63 +107,44 @@ function Get-UserToken {
     Write-Host "`n$($codeRequest.message)"
 
     $tokenBody = @{
-
         grant_type = "urn:ietf:params:oauth:grant-type:device_code"
         code       = $codeRequest.device_code
         client_id  = $clientId
-
     }
 
     # Get OAuth Token
     while ([string]::IsNullOrEmpty($tokenRequest.access_token)) {
-
         $tokenRequest = try {
-
             Invoke-RestMethod -Method POST -Uri "https://login.microsoftonline.com/$script:tenantId/oauth2/token" -Body $tokenBody
-
         }
         catch {
-
             $errorMessage = $_.ErrorDetails.Message | ConvertFrom-Json
-
             # If not waiting for auth, throw error
             if ($errorMessage.error -ne "authorization_pending") {
-
                 Throw
-
             }
-
         }
-
     }
-
     $script:token = $tokenRequest
-
 }
 
 function Refresh-UserToken {
     param (
-        
+
     )
 
     $refreshBody = @{
-
         client_id     = $script:clientId
         scope         = "$script:scope offline_access" # Add offline_access to scope to ensure refresh_token is issued
         grant_type    = "refresh_token"
         refresh_token = $script:token.refresh_token
-
     }
 
     $tokenRequest = try {
-
         Invoke-RestMethod -Method POST -Uri "https://login.microsoftonline.com/$script:tenantId/oauth2/token" -Body $refreshBody
-
     }
     catch {
-
         Throw
-    
     }
 
     $script:token = $tokenRequest
@@ -202,9 +153,7 @@ function Refresh-UserToken {
 
 function Get-Chats {
     param (
-
         [Parameter(mandatory = $true)][object]$User
-
     )
 
     Write-Host "Processing User: $($user.displayName) - $($user.mail)" -ForegroundColor Yellow
@@ -218,12 +167,9 @@ function Get-Chats {
     try {
         New-Item -ItemType Directory -Force -Path $fullPath | Out-Null
         Write-Host "SUCCESS" -ForegroundColor Green
-
     }
     catch {
-
         Write-Host "FAILED" -ForegroundColor Red
-
     }
 
     Write-Host " - Getting chats..." -NoNewline
@@ -231,24 +177,17 @@ function Get-Chats {
 
     # Loop through each chat thread and get messages, members etc.
     $chats.value | ForEach-Object {
-
         Write-Host " - Getting chat - $($_.id) - $($_.topic)..."
-
         # Get Members
         Write-Host "    - Getting chat members - $($_.id)..." -NoNewline
         $members = Invoke-GraphAPICall -URI "https://graph.microsoft.com/beta/$($user.Id)/chats/$($_.id)/members" -WriteStatus
         $chatMembers = $members.value.displayName -join ", "
-
         # Get Messages
         Write-Host "    - Getting chat messages - $($_.id)..." -NoNewline
         $messages = Invoke-GraphAPICall -URI "https://graph.microsoft.com/beta/$($user.Id)/chats/$($_.id)/messages" -WriteStatus
-
         $htmlMessages = "<br /><h3>Chat Transcript:</h3>"
-
         $messages.value | Sort-Object -Property createdDateTime | ForEach-Object {
-
             $important = Check-MessageImportance $_
-
             $htmlMessages += "
                             <div class='card'>
                                 <div class='card-header bg-light'><b>$($_.from.user.displayName)</b> $($_.createdDateTime)</div>
@@ -259,7 +198,6 @@ function Get-Chats {
                                 </div>
                                 "
             $htmlMessages += "</div><br />"
-
         }
 
         $html = "
@@ -288,31 +226,23 @@ function Get-Chats {
 "
         # Save file with topic name
         if ($_.topic) {
-
             $file = "Chat_$($_.topic)" -replace '([\\/:*?"<>|\s])+', "_"
-
-        # Or members if no topic
+            # Or members if no topic
         }
         else {
-
             $file = "Chat_$($members.value.displayName -join "_")" -replace '([\\/:*?"<>|\s])+', "_"
-
         }
-
         Write-Host "    - Saving chat to $fullPath/$file.htm... " -NoNewline
         Create-HTMLPage -Content $html -PageTitle "$($user.displayName) - Chat with $chatMembers" -Path "$fullPath/$file.htm"
-
     }
 
 }
 
 function Create-HTMLPage {
     param (
-
         [Parameter(mandatory = $true)][string]$Content,
         [Parameter(mandatory = $true)][string]$PageTitle,
         [Parameter(mandatory = $true)][string]$Path
-
     )
 
     $html = "
@@ -329,15 +259,11 @@ function Create-HTMLPage {
     </div>"
 
     try {
-            
         ConvertTo-Html -CssUri "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" -Body $html -Title $PageTitle | Out-File $Path
         Write-Host "SUCCESS" -ForegroundColor Green
-
     }
     catch {
-
         Write-Host "FAILED" -ForegroundColor Red
-
     }
 
 }
@@ -345,26 +271,20 @@ function Create-HTMLPage {
 function Check-MessageImportance {
 
     param (
-
         [Parameter(mandatory = $true)][System.Object]$message
-
     )
 
     if ($message.importance -eq "high") {
-
         return "<div class='alert alert-danger m-2' role='alert'>IMPORTANT!</div>"
-        
     }
     else {
-        
         return $null
-
     }
 
 }
 
 Write-Host "`n----------------------------------------------------------------------------------------------
-            `n Backup-TeamsChat.ps1 - Lee Ford - https://www.lee-ford.co.uk/
+            `n Backup-TeamsChat.ps1 - Lee Ford - https://github.com/leeford/Backup-TeamsChat
             `n----------------------------------------------------------------------------------------------" -ForegroundColor Yellow
 
 # Get Azure AD User Token
@@ -374,9 +294,6 @@ Get-UserToken | Out-Null
 $script:me = Invoke-GraphAPICall "https://graph.microsoft.com/v1.0/me"
 
 if ($script:me.id) {
-
     Write-Host "`nSIGNED-IN as $($me.mail)" -ForegroundColor Green
-
     $myChats = Get-Chats -User $me
-
 }
